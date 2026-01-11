98be43ce9818ffb5cb21421331f7dd27546a5ced
EllisOllier <ellisollier1@gmail.com>
2026-01-11 21:26:29 +0000

    Add getAlbumsByID endpoint

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type album struct {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -31,6 +32,22 @@ var albums = []album{
 // getAlbums response with the list of all albums as JSON
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// getAlbumByID locates the album whose ID value matches id
+// parameter sent by the client, then returns that album as a response
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for an album whose ID value
+	// matches the parameter
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 // postAlbums adds an album from JSON received in the request body
